49a107811e7b306dd43a5b8114282cf1334a2947
Asma Maria <asantaa31@gmail.com>
2024-11-12 01:41:47 +0600

    first commit

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,46 @@
 import React from "react";
-import employeeMSImage from "../assets/employee-ms.png";
+import BooksImage from "../assets/Book Club.png";
+import keymatch from "../assets/Secret Key Matcher.jpg"
+import HungrySpoon from "../assets/Hungry Food.png"
+import BooksBucket from "../assets/BooksBucket.png"
+import TechHub from "../assets/TechHub.jpg"
 import bookMSImage from "../assets/admin-dashboard.png";
 
 const projects = [
   {
     id: 1,
-    name: "Employee MS",
+    name: "Books Club",
     technologies: "MERN Stack",
-    image: employeeMSImage,
-    github: "https://github.com/YouafKhan1",
+    image: BooksImage,
+    github: "https://github.com/asma-maria/Simple-Book-Shop-Website",
   },
   {
     id: 2,
-    name: "Blog App",
+    name: "Secret Key Matcher",
     technologies: "MERN Stack",
-    image: bookMSImage,
-    github: "https://github.com/YouafKhan1",
+    image: keymatch,
+    github: "https://github.com/asma-maria/Secret-Key-Matcher.git",
   },
   {
     id: 3,
-    name: "Book MS",
+    name: "Hungry Spoon",
     technologies: "MERN Stack",
-    image: employeeMSImage,
-    github: "https://github.com/YouafKhan1",
+    image: HungrySpoon,
+    github: "https://github.com/asma-maria/Responsive-Food-Corner",
+  },
+  {
+    id: 4,
+    name: "Books Bucket",
+    technologies: "MERN Stack",
+    image: BooksBucket,
+    github: "https://github.com/asma-maria/Books-Bucket-Frontend",
+  },
+  {
+    id: 5,
+    name: "Tech Hub",
+    technologies: "MERN Stack",
+    image: TechHub,
+    github: "https://github.com/asma-maria/Course-Related-Website",
   },
 ];
 
@@ -30,7 +48,7 @@ const Projects = () => {
   return (
     <div className="bg-black text-white py-20" id="project">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
-        <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
+        <h2 className="text-4xl font-bold text-center mb-12">Recent Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
             <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
